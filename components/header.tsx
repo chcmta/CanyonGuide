f472b80df3fad7816966c656e47a30348e474221
kiko <kiko555@gmail.com>
2023-06-19 22:26:07 +0800

    調整場次41檢定內容內容

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,13 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const Header = () => {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/" className="hover:underline">
-        Blog
+        彰化縣登山協會溯溪嚮導訓練與檢定
       </Link>
-      .
     </h2>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
