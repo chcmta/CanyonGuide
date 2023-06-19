fe49f2ff42fd58fb125d5a175e96d10fc7c2c5fd
kiko <kiko555@gmail.com>
2023-06-19 22:39:41 +0800

    調整網站標題的呈現方式

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,19 +3,12 @@ import { CMS_NAME } from "../lib/constants";
 const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h2 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        彰化縣登山協會溯溪嚮導訓練與檢定
-      </h2>
-      {/* <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-blue-600 duration-200 transition-colors"
-        >
-          Next.js
-        </a>{" "}
-        and {CMS_NAME}.
-      </h4> */}
+      <h1 className="text-3xl md:text-5xl font-bold tracking-tighter leading-tight md:pr-8">
+        彰化縣登山協會
+      </h1>
+      <h4 className="text-center md:text-left text-3xl mt-5 md:pl-8">
+        溯溪嚮導訓練與檢定
+      </h4>
     </section>
   );
 };
